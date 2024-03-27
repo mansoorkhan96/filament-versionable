726d5b5fbfc943e713b9c964f41d8a6e81f1b7dd
Loan Besson <loan.besson@hotmail.fr>
2024-03-27 09:46:04 +0100

    Add french support

---
+++ resources/lang/fr/page.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'title' => 'Comparer les révisions de :title',
+    'breadcrumb' => 'Révisions',
+    'content_tab_label' => 'Révisions',
+    'revision_by' => 'Révision par :name',
+    'revisions_list' => 'Liste des révisions',
+];
