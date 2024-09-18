8c78bbab8734cfb2a7e929ec8e75fe86b35d0ab3
overtrue <anzhengchao@gmail.com>
2024-09-18 14:27:45 +0800

    docs: Add zh_* translations.

---
+++ resources/lang/zh_HK/actions.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'previous_version' => '上一個版本',
+    'next_version' => '下一個版本',
+    'restore' => [
+        'label' => '恢復到此版本',
+        'modal_description' => '你確定要恢復此版本嗎？',
+        'modal_submit_action_label' => '是的，恢復到此版本',
+    ],
+    'revisions' => '版本歷史',
+];
---
+++ resources/lang/zh_TW/actions.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'previous_version' => '上一版',
+    'next_version' => '下一版',
+    'restore' => [
+        'label' => '恢復至此版本',
+        'modal_description' => '您確定要恢復此版本嗎？',
+        'modal_submit_action_label' => '是的，恢復至此版本',
+    ],
+    'revisions' => '版本歷程',
+];
