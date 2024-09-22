2d306141483cb3907292ea11c1a60bfc19544454
Proovt@Arbeitstier <nico.luethi@datasurge.ch>
2024-09-22 19:36:16 +0200

    Added german translations

---
+++ resources/lang/de/actions.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'previous_version' => 'Vorherige',
+    'next_version' => 'Nächste',
+    'restore' => [
+        'label' => 'Diese Revision wiederherstellen',
+        'modal_description' => 'Bist du sicher, dass du diese Revision wiederherstellen möchtest?',
+        'modal_submit_action_label' => 'Ja, wiederherstellen',
+    ],
+    'revisions' => 'Revisionen',
+];
