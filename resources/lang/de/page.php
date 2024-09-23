b3783f6cd9906a02300c4b3da75303402f3642b3
Proovt <47065617+Proovt@users.noreply.github.com>
2024-09-23 07:40:24 +0200

    Added german translations (#26)

---
+++ resources/lang/de/page.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'title' => 'Vergleiche die Revisionen von :title',
+    'breadcrumb' => 'Revisionen',
+    'content_tab_label' => 'Revisionen',
+    'revision_by' => 'Revision von :name',
+    'revisions_list' => 'Revisionsliste',
+    'anonymous_user' => 'Anonymer Benutzer',
+];
