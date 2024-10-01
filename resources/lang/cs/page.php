a196f3868c081e1b84636dfc8a4b1057127f77a2
Jarka Pachlová <jarka.p@gmail.com>
2024-10-01 18:41:09 +0200

    Add czech translations

---
+++ resources/lang/cs/page.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'title' => 'Porovnání revizí :title',
+    'breadcrumb' => 'Revize',
+    'content_tab_label' => 'Revize',
+    'revision_by' => 'Revize od :name',
+    'revisions_list' => 'Seznam revizí',
+    'anonymous_user' => 'Anonymní uživatel',
+];
