b3afe12127b6320250a66dbbff830ebeb5259b46
a21ns1g4ts <a21ns1g4ts@gmail.com>
2025-12-02 10:14:35 -0300

    feat: Add Portuguese (pt and pt_BR) translations for page and action elements.

---
+++ resources/lang/pt_BR/actions.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'previous_version' => 'Anterior',
+    'next_version' => 'Próximo',
+    'restore' => [
+        'label' => 'Restaurar esta Revisão',
+        'modal_description' => 'Tem certeza de que deseja restaurar esta versão?',
+        'modal_submit_action_label' => 'Sim, restaurar',
+    ],
+    'revisions' => 'Revisões',
+];
