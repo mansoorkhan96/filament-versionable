e0645af02045d080dc625e4ad6cb2a754ae8e731
Danvi S.c <atila.danvi@outlook.com>
2025-12-03 01:50:01 -0300

    feat: Add Portuguese (pt and pt_BR) translations for page and action elements. (#48)
    Co-authored-by: a21ns1g4ts <a21ns1g4ts@gmail.com>

---
+++ resources/lang/pt_BR/page.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'breadcrumb' => 'Revisões',
+    'content_tab_label' => 'Revisões',
+    'revision_by' => 'Revisão por :name',
+    'revisions_list' => 'Lista de revisões',
+    'anonymous_user' => 'Usuário Anônimo',
+];
